8a6fcb32b9e1cd7bdbd855b908ce7824272a4ff5
GAMALIEL D'MELLO <102117407@student.swin.edu.au>
2019-10-22 22:52:37 +1100

    Added Salary Function Added

--- Week9/9.2/9.2/9.2.cpp
+++ Week9/9.2/9.2/9.2.cpp
@@ -6,7 +6,7 @@ using namespace std;
 struct Emp getdata(); //gets emp dataand reads into the array
 void printdata(struct Emp emp[], int size); //prints out the data in the struct onto terminal
 double get_average(struct Emp* emp, int size , string cmp_name); //average salary of a company
-double get_salary(struct Emp emp[], string emp_name); //serach the salary of an employee
+double get_salary(struct Emp emp[], int size, string emp_name); //serach the salary of an employee
 
 
 struct company_detail
@@ -29,7 +29,9 @@ int main()
 	int i;
 	struct Emp emp[size];
 	double average;
+	double salary;
 	string cmp_name;
+	string emp_name;
 	
 	for (i = 0; i<size;i++) 
 	{
@@ -42,7 +44,12 @@ int main()
 	cin >> cmp_name;
 	average = get_average(emp, size, cmp_name);
 
+	cout << "Enter Employee name to get Employee Salary ";
+	cin >> emp_name;
+	salary = get_salary(emp, size, emp_name);
+
 	cout << "Average For " << cmp_name << " is " << average << endl;
+	cout << "Salary For " << emp_name << " is " << salary << endl;
 
 
 	return 0;
@@ -103,4 +110,16 @@ double get_average(struct Emp* emp, int size, string cmp_name)
 	}
 
 	return average / count;
+}
+
+double get_salary(struct Emp emp[],int size,  string emp_name)
+{
+	int i;
+	for (i = 0; i < size;i++)
+	{
+		if (emp[i].emp_name == emp_name)
+		{
+			return emp[i].salary;
+		}
+	}
 }
