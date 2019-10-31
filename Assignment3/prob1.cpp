31d41e7ce826e3cfe5b3a5254e649dbaf0bd241e
Gamaliel D'mello <gamalieldmello99@gmail.com>
2019-10-31 23:24:29 +1100

    Update prob1.cpp

--- Assignment3/prob1.cpp
+++ Assignment3/prob1.cpp
@@ -6,226 +6,188 @@
 using namespace std;
 
 struct student
-{   
-	string name;   
-	int id;   
-	float grade; 
+{
+	string name;
+	int id;
+	float grade;
+	student* next;
 };
 typedef struct student stu;
+typedef stu* stuptr;
 
-int readFile(stu students[]);
+void readFile(stuptr students);
 void updateFile();
-void sort_student_grades(stu students[], int student_total);
-void sort_student_names(stu students[], int student_total);
-void print_details(stu students[], int student_total);
-int search_s(stu students[], int student_total, string search);
-void find_max(stu students[],int student_total);
-void average(stu students[], int student_total);
+void print_details(stuptr students);
+void search_s(stuptr students, string search);
+void find_max(stuptr students);
+void average(stuptr students);
 void menu();
 
 int main()
 {
-  const int size = 100;
-  stu students[size];
-  int student_total = 0;
-  int choice;
-	float index;
-  string search;
+	int choice;
+	string search;
+	stuptr start = new student;
 
-  student_total = readFile(students);
-  do{
-    menu();
+	readFile(start);
+
+	do {
+		menu();
 		cout << "What is your choice?  ";
-        cin >> choice;  
-        cout << endl;
-		switch(choice){
-			case 1:
-				print_details(students, student_total);
-				break;
-			case 2:
-      average(students,student_total);
-      break;
-      case 3:
-      cout<<"sort by (1)grade or (2)name"<<endl;
-      cin>>choice;
-      switch(choice)
-      {
-        case 1:
-        sort_student_grades(students, student_total);
-        break;
-        case 2:
-        sort_student_names(students, student_total);
-        break;
-      }
-      break;
-      case 4:
-      
-      cout << "Enter student's grade you want to look for: ";
-      cin >> search; 
-      cout << endl;
-				index = search_s(students, student_total, search);
-					if(index == -1)
-                    {
-						cout << "No student found ";
-                        cout << endl;
-					} 
-				break;
-        case 5: 
-			find_max(students,student_total);
-				break;
-			case 6: 
+		cin >> choice;
+		cout << endl;
+		switch (choice) {
+		case 1:
+			print_details(start);
+			break;
+		case 2:
+			average(start);
+			break;
+		case 3:
+
+			cout << "Enter student's grade you want to look for: ";
+			cin >> search;
+			cout << endl;
+			search_s(start, search);
+			break;
+		case 4:
+			find_max(start);
+			break;
+		case 5:
 			updateFile();
-      student_total = readFile(students);
-      break;
-      case 7:
-      cout<<"Program ended Succesfull"<<endl;
-      break;
-    }
-  } while (choice != 7);
-return 0;
+			readFile(start);
+			break;
+		case 6:
+			cout << "Program ended Succesfull" << endl;
+			break;
+		}
+	} while (choice != 7);
+	return 0;
 }
 
-int readFile(stu students[])
+void readFile(stuptr start)
 {
 	int i = 0;
+	stuptr students = new student;
+	stuptr previous = NULL;
+
 
 	string line;
 	ifstream inFile("student_details.txt");
 
-	while (!inFile.eof())
-  {
-  inFile >> students[i].name >> students[i].id >>students[i].grade; 
-  i++;
-  }
+	if (inFile.fail())
+	{
+		cout << endl;
+		cout << "File Not Found" << endl;
+	}
 
-inFile.close();
-return i;
+	inFile >> start->name >> start->id >> start->grade;
+	start->next = students;
+
+
+	while (!inFile.eof())
+	{
+		inFile >> students->name >> students->id >> students->grade;
+		students->next = NULL;
+		previous = students;
+		students = new student;
+		previous->next = students;
+	}
+
+	inFile.close();
 }
 
 void updateFile()
 {
-  string in_name;
-  int in_id;
-  float in_grade;
+	string in_name;
+	int in_id;
+	float in_grade;
 
-  ofstream outFile;
-	outFile.open("student_details.txt",ios_base::app);
+	ofstream outFile;
+	outFile.open("student_details.txt", ios_base::app);
 
-  cout<<"Enter Details"<<endl;
-  cout<<"Name: ";
-  cin>>in_name;
+	cout << "Enter Details" << endl;
+	cout << "Name: ";
+	cin >> in_name;
 
-  cout<<"ID: ";
-  cin>>in_id;
+	cout << "ID: ";
+	cin >> in_id;
 
-  cout<<"Grade: ";
-  cin>>in_grade;
+	cout << "Grade: ";
+	cin >> in_grade;
 
-  outFile<<endl<<in_name<<" "<<in_id<<" "<<in_grade;
-  cout<<"Succesfull Updated File"<<endl;
-outFile.close();
+	outFile << endl << in_name << " " << in_id << " " << in_grade;
+	cout << "Succesfull Updated File" << endl;
+	outFile.close();
 }
 
-void sort_student_grades(stu students[], int student_total)
+void print_details(stuptr students)
 {
-  int i = 0;
-  int j= i+1;
-  for(i=0;i<student_total-1;i++) 
+ while (students->next != NULL)
+	{
+		cout << "Name: " << students->name << endl;
+		cout << "ID: " << students->id << endl;
+		cout << "Grade: " << students->grade << endl;
+		cout << endl;
+		students = students->next;
+}
+}
+
+void search_s(stuptr students, string search)
+{
+  while (students -> next != NULL)
   {
-		for(j = i+1;j<student_total;j++)
-    {			
-			if(students[i].grade > students[j].grade)
-      { 
-				swap(students[i].name , students[j].name);
-				swap(students[i].id , students[j].id);
-        swap(students[i].grade , students[j].grade);
-			}
-		}
-  }
- print_details(students, student_total);
-}
-
-void sort_student_names(stu students[], int student_total)
-{
-  int i = 0;
-  int j= i+1;
-  for(i=0;i<student_total-1;i++) 
+		if(students->name == search) 
+    {
+		 cout << "Name: " << students->name << endl;
+		 cout << "ID: " << students->id << endl;
+		 cout << "Grade: " << students->grade << endl;
+		 cout << endl;   
+		} 
+    students = students->next;	
+	}
+  if (students -> next == NULL)
   {
-		for(j = i+1;j<student_total;j++)
-    {			
-			if(students[i].name > students[j].name)
-      { 
-				swap(students[i].name , students[j].name);
-				swap(students[i].id , students[j].id);
-        swap(students[i].grade , students[j].grade);
-			}
-		}
+    cout<<"No Student Found"<<endl;
   }
- print_details(students, student_total);
+
 }
 
-void print_details(stu students[], int student_total)
+void find_max(stuptr students)
 {
-  int i = 0;
-   for(i=0;i<student_total;i++)
+	int max = 0;
+  while (students->next != NULL)
+	{
+		if(students->grade > max)
     {
-        cout <<"Name: " << students[i].name << endl;
-        cout <<"ID: " << students[i].id << endl;
- 
-        cout <<"Grade: " <<students[i].grade << endl;
-        cout << endl;		
-	  }
-}
-
-int search_s(stu students[], int student_total, string search)
-{
-  int i;
-		for(i=0;i<student_total;i++) 
-    {
-        if (students[i].name == search)
-        {	
-            cout<<"Name: " << students[i].name << endl;
-            cout <<"ID: " << students[i].id << endl;
-            cout <<"Grade: " <<students[i].grade << endl;
-            cout << endl;
-            break;
-        } 	
-	}
-    return 1;
-}
-
-void find_max(stu students[],int student_total)
-{
-  float max=0;
-  int i = 0;
-	for(i =0;i<student_total;i++)
-    {
-		if(students[i].grade > max)
-		max = students[i].grade;
-	}
+      		max = students->grade;
+    }
+	students = students->next;  
+  }
 	cout << "Highest Grade is: " << max << endl << endl;
 }
 
-void average(stu students[], int student_total)        
+void average(stuptr students)
 {
-	float avg;
-  float sum=0;
-  int i;
-	for(int i=0;i<student_total;i++)
-    {
-		sum += students[i].grade;
+	float avg = 0;
+  int i = 0;
+  while (students->next != NULL)
+	{
+		
+		avg +=students->grade;
+    i++;
+		students = students->next;
 	}
-	avg = sum /student_total;
+  avg = avg/i;
 	cout << "Average Grade is: " << avg << endl << endl;
 }
 
 void menu()
 {
-  	cout << "(1) Display students	" << endl;
-		cout << "(2) Calculate average " << endl;
-		cout << "(3) Sort the students' details " << endl;
-		cout << "(4) Search for particular student's grade " << endl;
-		cout << "(5) Find maximum " << endl;
-		cout << "(6) Add new student to the Record" << endl;
-		cout << "(7) Quit Program" << endl;
+	cout << "(1) Display students	" << endl;
+	cout << "(2) Calculate average " << endl;
+	cout << "(3) Search for particular student's grade " << endl;
+	cout << "(4) Find maximum " << endl;
+	cout << "(5) Add new student to the Record" << endl;
+	cout << "(6) Quit Program" << endl;
 }
