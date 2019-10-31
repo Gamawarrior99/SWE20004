0575adf2c1b38219f725e1f3c51b764386cfa191
Gamaliel D'mello <gamalieldmello99@gmail.com>
2019-10-31 20:07:49 +1100

    Add files via upload

---
+++ Assignment3/prob2.cpp
@@ -0,0 +1,177 @@
+#include<iostream>
+#include<string>
+#include<cstdlib>
+#include<fstream>
+
+using namespace std;
+
+struct student
+{   
+	string name;   
+	int id;   
+	float grade;
+  student* next;
+};
+typedef struct student stu;
+typedef stu* stuptr;
+
+int readFile(stuptr students);
+void updateFile();
+void sort_student_grades(stuptr students);
+void sort_student_names(stuptr students);
+void print_details(stuptr students);
+int search_s(stuptr students, string search);
+void find_max(stuptr students);
+void average(stuptr students);
+void menu();
+
+int main()
+{
+  
+  int choice;
+	float index;
+  string search;
+  stuptr students;
+
+  do{
+    menu();
+		cout << "What is your choice?  ";
+        cin >> choice;  
+        cout << endl;
+		switch(choice){
+			case 1:
+				print_details(students);
+				break;
+			case 2:
+      average(students);
+      break;
+      case 3:
+      cout<<"sort by (1)grade or (2)name"<<endl;
+      cin>>choice;
+      switch(choice)
+      {
+        case 1:
+        sort_student_grades(students);
+        break;
+        case 2:
+        sort_student_names(students);
+        break;
+      }
+      break;
+      case 4:
+      
+      cout << "Enter student's grade you want to look for: ";
+      cin >> search; 
+      cout << endl;
+				index = search_s(students, search);
+					if(index == -1)
+                    {
+						cout << "No student found ";
+                        cout << endl;
+					} 
+				break;
+        case 5: 
+			find_max(students);
+				break;
+			case 6: 
+			updateFile();
+      readFile(students);
+      break;
+      case 7:
+      cout<<"Program ended Succesfull"<<endl;
+      break;
+    }
+  } while (choice != 7);
+return 0;
+}
+
+int readFile(stuptr students)
+{
+	int i = 0;
+
+	string line;
+	ifstream inFile("student_details.txt");
+
+  if(inFile.fail())
+    {
+		cout << endl;
+        cout << "File Not Found" << endl;
+	} 
+
+	while (!inFile.eof())
+  {
+    inFile >> students->name >> students->id>>students->grade; 
+	  students = new stu;
+	  students = students->next;
+  }
+
+inFile.close();
+return i;
+}
+
+void updateFile()
+{
+  string in_name;
+  int in_id;
+  float in_grade;
+
+  ofstream outFile;
+	outFile.open("student_details.txt",ios_base::app);
+
+  cout<<"Enter Details"<<endl;
+  cout<<"Name: ";
+  cin>>in_name;
+
+  cout<<"ID: ";
+  cin>>in_id;
+
+  cout<<"Grade: ";
+  cin>>in_grade;
+
+  outFile<<endl<<in_name<<" "<<in_id<<" "<<in_grade;
+  cout<<"Succesfull Updated File"<<endl;
+outFile.close();
+}
+
+void sort_student_grades(stuptr students)
+{
+}
+
+void sort_student_names(stuptr students)
+{
+  
+}
+
+void print_details(stuptr students)
+{
+
+}
+
+int search_s(stuptr students, string search)
+{
+    return 1;
+}
+
+void find_max(stu students)
+{
+  int max;
+	cout << "Highest Grade is: " <<max<< endl << endl;
+}
+
+void average(stuptr students)        
+{
+	float avg;
+  
+	cout << "Average Grade is: " << avg << endl << endl;
+}
+
+void menu()
+{
+  	cout << "(1) Display students	" << endl;
+		cout << "(2) Calculate average " << endl;
+		cout << "(3) Sort the students' details " << endl;
+		cout << "(4) Search for particular student's grade " << endl;
+		cout << "(5) Find maximum " << endl;
+		cout << "(6) Add new student to the Record" << endl;
+		cout << "(7) Quit Program" << endl;
+}
